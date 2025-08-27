8003aaaf53bbf9e7accb6c012bf3fa840bf1832f
Kim EungJin <mmm806@naver.com>
2025-08-27 12:07:21 +0900

    :tada: init Spring Boot project

---
+++ src/test/java/com/example/mori/MoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
